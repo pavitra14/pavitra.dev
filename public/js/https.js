435b670d5f132a031fae1abcaf1f92e969c1482a
Pavitra Behre <pavitra.behre@gmail.com>
2021-03-11 03:28:23 +0530

    Force HTTPS

--- public/js/https.js
+++ public/js/https.js
@@ -2,7 +2,7 @@ if (
   location.protocol !== "https:" &&
   window.location.hostname.match(
     /localhost|[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{2,3}|::1|\.local|^$/gi
-  ) != "localhost"
+  ) == null
 ) {
   location.replace(
     `https:${location.href.substring(location.protocol.length)}`
