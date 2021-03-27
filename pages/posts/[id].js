74263ddb6a1565364b8275af7059486a27b207e3
Pavitra Behre <pavitra.behre@gmail.com>
2021-03-27 10:59:05 +0530

    Added new Blog Added prismjs code highlighting Added Buy me a coffee button on blog pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,8 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import "prismjs/themes/prism.css";
+import Image from "next/image";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -37,6 +39,15 @@ export default function Post({ postData }) {
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
+      <a href="https://www.buymeacoffee.com/HOZtmqr" target="_blank">
+        <Image
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
+          alt="Buy Me A Coffee"
+          height={60}
+          width={217}
+          unoptimized={true}
+        />
+      </a>
     </Layout>
   );
 }
