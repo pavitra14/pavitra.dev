9da982d0cecb8ea5bb7cb101b62d4d7467d580b9
Pavitra Behre <behrepav@amazon.com>
2023-07-06 12:56:29 +0530

    Added VERCEL_URL to resolve CORS issues.

--- constants/constants.js
+++ constants/constants.js
@@ -5,9 +5,14 @@ export default class Constants{
     static SHORT_INTRO = "Software Development Engineer @ Amazon 🇮🇳";
 
     static REMOTE_URL = "https://pavitra.dev";
+    static VERCEL_URL = "https://pavitra-dev.vercel.app";
     static DEV_URL = "http://localhost:3000";
 
     static BASE_URL = () => {
+        if(process.env.VERCEL) {
+            console.log("Application is hosted via vercel.")
+            return this.VERCEL_URL;
+        }
         if(process.env.mode == "DEV") {
             return this.DEV_URL;
         }
