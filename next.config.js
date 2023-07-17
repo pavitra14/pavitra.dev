3187ec6d5a6f56b6b689a33227977acc8cd4cc80
Pavitra Behre <behrepav@amazon.com>
2023-07-17 23:07:04 +0530

    Allow nextjs to use Vercel Env in browser

--- next.config.js
+++ next.config.js
@@ -15,6 +15,9 @@ module.exports = (phase) => {
     },
     env: {
       mode: "",
+      UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+      UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+      VERCEL: process.env.VERCEL
     },
   };
 
